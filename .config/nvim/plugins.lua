4b94a0a24c3d17d2412f469e72462df342250a1c
czlabinger <czlabinger@student.tgm.ac.at>
2025-03-24 13:49:30 +0100

    Added neovim config

---
+++ .config/nvim/plugins.lua
@@ -0,0 +1,165 @@
+return {
+  -- Plugin manager
+  { "folke/lazy.nvim" },
+
+  -- Completion and snippets
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "saadparwaiz1/cmp_luasnip",
+      "L3MON4D3/LuaSnip",
+      "rafamadriz/friendly-snippets",
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+          { name = 'buffer' },
+          { name = 'path' },
+        }),
+      })
+    end,
+  },
+
+  -- LSP and related tools
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "folke/neodev.nvim",
+      "hrsh7th/cmp-nvim-lsp", -- Ensure cmp-nvim-lsp is listed here
+    },
+    config = function()
+      require("mason").setup()
+      require("mason-lspconfig").setup()
+      local lspconfig = require("lspconfig")
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+      -- Setup language servers here
+      local servers = { 'lua_ls', 'rust_analyzer', 'pyright', 'tsserver' }
+      for _, lsp in ipairs(servers) do
+        lspconfig[lsp].setup {
+          capabilities = capabilities,
+        }
+      end
+    end,
+  },
+
+  -- Treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "lua", "vim", "vimdoc", "query", "javascript", "typescript", "rust", "python", "css", "html", "latex", "norg", "scss", "svelte", "tsx", "typst", "vue" },
+        highlight = { enable = true },
+      })
+    end,
+  },
+
+  -- File explorer
+  {
+    "kyazdani42/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("nvim-tree").setup()
+    end,
+  },
+
+  -- Fuzzy finder
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup()
+    end,
+  },
+
+  -- Git integration
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+    end,
+  },
+
+  -- Status line
+  {
+    "nvim-lualine/lualine.nvim",
+    config = function()
+      require("lualine").setup()
+    end,
+  },
+
+  -- Auto pairs
+  {
+    "windwp/nvim-autopairs",
+    config = function()
+      require("nvim-autopairs").setup()
+    end,
+  },
+
+  -- Comment toggler
+  {
+    "numToStr/Comment.nvim",
+    config = function()
+      require("Comment").setup()
+    end,
+  },
+
+  -- Color scheme
+  {
+    "folke/tokyonight.nvim",
+    config = function()
+      vim.cmd[[colorscheme tokyonight]]
+    end,
+  },
+
+  -- Which key
+  {
+    "folke/which-key.nvim",
+    config = function()
+      require("which-key").setup()
+    end,
+  },
+
+  -- Additional plugins without specific configurations
+  { "MunifTanjim/nui.nvim" },
+  { "rcarriga/nvim-dap-ui" },
+  { "mfussenegger/nvim-dap" },
+  { "tamago324/nlsp-settings.nvim" },
+  { "nvimtools/none-ls.nvim" },
+  { "Tastyep/structlog.nvim" },
+  { "nvim-lua/popup.nvim" },
+  { "JoosepAlviste/nvim-ts-context-commentstring" },
+  { "tamago324/lir.nvim" },
+  { "ahmedkhalf/project.nvim" },
+  { "SmiteshP/nvim-navic" },
+  { "akinsho/bufferline.nvim" },
+  { "goolord/alpha-nvim" },
+  { "akinsho/toggleterm.nvim" },
+  { "b0o/schemastore.nvim" },
+  { "RRethy/vim-illuminate" },
+  { "lukas-reineke/indent-blankline.nvim" },
+  { "folke/todo-comments.nvim" },
+  { "NeogitOrg/neogit" },
+}
+
